fe84c1e90e685390ec60b612873ed63b82b18907
David <david.maruhn@stud.th-deg.de>
2025-12-07 22:35:41 +0100

    style: format code for consistency and readability in data-source.ts

--- src/backend/data-source.ts
+++ src/backend/data-source.ts
@@ -8,26 +8,36 @@ import { GameParticipant } from './entity/GameParticipant';
 
 const AppDataSource = new DataSource({
   type: 'mariadb',
-  host: process.env.DB_HOST ,
-  port:  Number(process.env.DB_PORT) ,
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  entities: [ Game, GameParticipant],
+  entities: [Game, GameParticipant],
   synchronize: true,
   // optional but explicit:
   connectorPackage: 'mysql2',
 });
 
-class DataBase{
+class DataBase {
   datasource: DataSource;
 
-  constructor(){
+  constructor() {
     this.datasource = AppDataSource;
   }
-  async initialize(){
+  async initialize() {
     return await this.datasource.initialize();
   }
+  async createGame() {
+    const gameRepo = this.datasource.getRepository(Game);
+    const newGame = gameRepo.create();
+    newGame.name = `Game in channel ${interaction.channelId}`;
+    newGame.gamemaster = interaction.user.id;
+    newGame.timestamp = new Date();
+    newGame.duration = 30; // default duration
+    await gameRepo.save(newGame);
+  }
+
 }
 const dbInstance = new DataBase();
 await dbInstance.initialize();
