ee32d2a83d529e9bb5c37dc4a43a92171570f7c6
David <david.maruhn@stud.th-deg.de>
2025-12-08 22:56:31 +0100

    feat: update GameLogic class with initial method structure for game management

--- src/backend/gamelogic.ts
+++ src/backend/gamelogic.ts
@@ -1,13 +1,24 @@
+import { Game, GameState } from "./entity/Game";
 
 
 class GameLogic {
+    game?: Game;
 
-    startGame(playerID:string){
+    startGame(playerID: string) {
+
 
     }
-    addParticipant(playerID:string){
+    addParticipant(playerID: string) {
+        // only if he doesn't exist already
+        if(this.game?.state == GameState.Waiting4Players) {
+            
+        }
     }
-    removeParticipant(playerID:string){
+    removeParticipant(playerID: string) {
+        // only if he exists
+    }
+    sendWord2AllUsers() {
+
     }
 
 }
